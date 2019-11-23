23aec041b75a1f8c05a5585980734a7530ac3b6d
Peter Jones <kephail@me.com>
2019-11-23 22:26:35 +0000

    Create/use factory function to create a shallowWrapper in tests

--- src/App.test.js
+++ src/App.test.js
@@ -2,24 +2,36 @@ import React from 'react';
 import Enzyme, { shallow } from 'enzyme';
 import EnzymeAdapter from 'enzyme-adapter-react-16';
 import App from './App';
-import { exportAllDeclaration } from '@babel/types';
 
 Enzyme.configure({ adapter: new EnzymeAdapter() })
 
+/**
+ * 
+ * Factory function to create a shallowWrapper 
+ * for the App component
+ * @function setup
+ * @param {object} props - Component props specific to this setup.
+ * @param {*} state - initial state for setup.
+ * @returns {shallowWrapper}
+*/
+const setup = (props = {}, state = null) => {
+  return shallow(<App {...props} />)
+}
+
 it('renders without crashing', () => {
-  const wrapper = shallow(<App />);
+  const wrapper = setup();
   const appComponent = wrapper.find("[data-test='component-app']");
   expect(appComponent.length).toBe(1);
 });
 
 it('renders increment button', () => {
-  const wrapper = shallow(<App />);
+  const wrapper = setup();
   const button = wrapper.find("[data-test='increment-button']");
   expect(button.length).toBe(1);
 });
 
 it('renders counter display', () => {
-  const wrapper = shallow(<App />);
+  const wrapper = setup();
   const counterDisplay = wrapper.find("[data-test='counter-display']");
   expect(counterDisplay.length).toBe(1);
 });
