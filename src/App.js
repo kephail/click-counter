356c3b982d7314d142ef86247dd72f44015acaa3
Peter Jones <kephail@me.com>
2019-11-23 23:13:58 +0000

    button functionality

--- src/App.js
+++ src/App.js
@@ -2,18 +2,27 @@ import React, { Component } from "react";
 import "./App.css";
 
 class App extends Component {
-  constructor (props) {
+  constructor(props) {
     super(props);
 
     this.state = {
-      count: 0,
-    }
+      count: 0
+    };
   }
   render() {
     return (
       <div data-test="component-app">
-        <h1 data-test="counter-display">The count is currently: 0</h1>
-        <button data-test="increment-button">Increment Counter</button>
+        <h1 data-test="counter-display">
+          The count is currently: {this.state.count}
+        </h1>
+        <button
+          data-test="increment-button"
+          onClick={() => {
+            this.setState({ count: this.state.count + 1 });
+          }}
+        >
+          Increment Counter
+        </button>
       </div>
     );
   }
