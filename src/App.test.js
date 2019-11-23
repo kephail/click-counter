1ce7cb7c263264ecd77bb247b39d94fa70cdc6db
Peter Jones <kephail@me.com>
2019-11-23 22:41:25 +0000

    findByTestAttr utility function

--- src/App.test.js
+++ src/App.test.js
@@ -18,21 +18,32 @@ const setup = (props = {}, state = null) => {
   return shallow(<App {...props} />)
 }
 
+/**
+ * Return shallowWrapper containing node(s)
+ * with the given data-test value.
+ * @param {shallowWrapper} wrapper - Enzyme shallow wrapper to search within.
+ * @param {string} val - Value of the data-test attribute for search.
+ * @returns {shallowWrapper}
+ */
+const findByTestAttr = (wrapper, val) => {
+  return wrapper.find(`[data-test="${val}"]`)
+}
+
 it('renders without crashing', () => {
   const wrapper = setup();
-  const appComponent = wrapper.find("[data-test='component-app']");
+  const appComponent = findByTestAttr(wrapper, 'component-app');
   expect(appComponent.length).toBe(1);
 });
 
 it('renders increment button', () => {
   const wrapper = setup();
-  const button = wrapper.find("[data-test='increment-button']");
+  const button = findByTestAttr(wrapper, 'increment-button');
   expect(button.length).toBe(1);
 });
 
 it('renders counter display', () => {
   const wrapper = setup();
-  const counterDisplay = wrapper.find("[data-test='counter-display']");
+  const counterDisplay = findByTestAttr(wrapper, 'counter-display');
   expect(counterDisplay.length).toBe(1);
 });
 
